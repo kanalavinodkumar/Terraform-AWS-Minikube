31c6cd8bcb49e38769a7891a038f99d59c3561e0
kanalavinodkumar <kanalavinodkumar@gmail.com>
2023-11-02 14:42:11 +0530

    workstation

---
+++ Workstation.tf
@@ -0,0 +1,9 @@
+module "this" {
+  source = "git::https://github.com/kanalavinodkumar/Docker-infra.git?ref=master"
+  cidr_block = var.cidr_block
+  common_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  public_subnet_name = var.public_subnet_name
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_name = var.private_subnet_name
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "cidr_block" {
+  type = string
+  default = "10.10.0.0/16"
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {
+        Name = "Minikube"
+        Environment = "PROD"
+        Terraform = "True"
+    }
+}
+
+variable "public_subnet_name" {
+  type = list
+  default = ["subnet-1a", "subnet-1b"]
+}
+
+variable "public_subnet_cidr" {
+  type = list
+  default = ["10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "private_subnet_name" {
+  default = ["subnet-1c", "subnet-1d"]
+}
+
+variable "private_subnet_cidr" {
+  default = ["10.10.3.0/24", "10.10.4.0/24"]
+}
+
+variable "security_group_name" {
+  type = string
+  default = "Terraform-SG"
+}
